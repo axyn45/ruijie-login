e0b0e2021d16b0429376aeebddd9be7783a06e11
axyn45 <axyn45@outlook.com>
2024-03-13 14:58:27 +0800

    Added shell version

---
+++ connect.sh
@@ -0,0 +1,13 @@
+#! /bin/ash
+#timestamp=$(echo "----------------------------------------------------------------";date)
+
+ping -c3 223.5.5.5
+code=$?
+echo $pingcode
+if [ $code -ne 0 ]
+then
+        python /root/autoconnect/connect.py
+else
+        echo "Good Connection"
+
+fi
